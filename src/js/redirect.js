1853d5d14289986758f9001de34336511fd2ecb3
Betoe <devrenjbr@gmail.com>
2024-12-01 18:25:50 -0300

    chore: Adicionando links

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -24,6 +24,10 @@ async function redirecionar(site) {
     desconews: { url: 'https://wa.me/5521997927656', desc: 'Descomplica: Whatsapp Desconews' },
     live_pex: { url: 'https://livestream.com/accounts/8954674/events/11194068/player', desc: 'Descomplica: Live Projeto de Extensão' },
     mural_pex: { url: 'https://tinyurl.com/descomplicados-mural-pex', desc: 'Descomplicados: Mural dos Projeto de Extensão' },
+    gabarito_drive: { url: 'https://docs.google.com/spreadsheets/d/1qso-vckMjekNxqr0nEUBhA9dytBuqo2f4X8kM6wqTIE/edit#gid=1119658359', desc: 'Google Drive: Gabarito' },
+    gabarito_discord1: { url: 'https://discord.gg/jvjzcXjtdG', desc: 'Discord: ADS - Descomplica' },
+    gabarito_discord2: { url: 'https://discord.gg/RtSBvm3HQ3', desc: 'Discord: ADS 2023' },
+    gabarito_discord3: { url: 'https://discord.gg/6DbP5vx5mt', desc: 'Discord: ADS 2023.4' },
     default: () => {
       document.body.style.opacity = 0;
       alert('Site não encontrado, tente novamente')
